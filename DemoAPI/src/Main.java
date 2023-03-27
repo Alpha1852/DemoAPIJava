7362334dadc03557fbb5522cfbd42ecea3176d10
ALPHAHP\alpha <alpha-ibrahima.diallo@epita.fr>
2023-03-27 11:40:36 +0200

    first commit

--- DemoAPI/src/Main.java
+++ DemoAPI/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Cast et transtypage");
+
+          int i  = (int) 0.999;
+          System.out.println(i);
     }
 }
