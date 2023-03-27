697996c3528fb538880ab87a0a256c249991cd86
ALPHAHP\alpha <alpha-ibrahima.diallo@epita.fr>
2023-03-27 14:49:40 +0200

    Commit: Enumerations

---
+++ epita-saison7/src/com/formation/epita/bnpp/enumeration/ex1/JourSemaine.java
@@ -0,0 +1,14 @@
+package com.formation.epita.bnpp.enumeration.ex1;
+
+public enum JourSemaine {
+    DIMANCHE,
+    LUNDI,
+    MARDI,
+    MERCREDI,
+    JEUDI,
+    VENDREDI,
+    SAMEDI;
+
+
+
+}
---
+++ epita-saison7/src/com/formation/epita/bnpp/enumeration/ex1/MainEnum.java
@@ -0,0 +1,9 @@
+package com.formation.epita.bnpp.enumeration.ex1;
+
+public class MainEnum {
+
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+
+    }
+}
